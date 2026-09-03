87be6a0b517d7f7bb1e6936f42a5f5697d31a3a1
katestan <kate.stanley@crick.ac.uk>
2026-09-03 15:08:57 +0100

    Fix 5' end-motif extraction for forward-strand reads

--- get_end_motifs.sh
+++ get_end_motifs.sh
@@ -2,7 +2,7 @@
 
 #Explanation:
 
-#samtools view -f 2 -F 268 -q 1 input.bam: This command filters the BAM file to keep only the properly paired reads, with both mates mapped to the same chromosome, with mapping quality at least 1, and excludes secondary and supplementary alignments. The -F 268 flag is a combination of the flags for unmapped reads (4), mates unmapped (8), read is PCR or optical duplicate (256), totaling 268.
+#samtools view -f 2 -F 284 -q 1 input.bam: This command filters the BAM file to keep only the properly paired reads, with both mates mapped to the same chromosome, with mapping quality at least 1, and excludes secondary and supplementary alignments. The -F 284 flag is a combination of the flags for unmapped reads (4), mates unmapped (8), not primary alignement (256), read reverse strand (16) totaling 284.
 
 #awk '$9<=600 && $9>=-600': This awk command further filters the alignments to keep only those with an insert size less than or equal to 600 bases.
 
@@ -16,15 +16,14 @@
 
 #uniq -c: This command counts the unique 4-mers in the sorted list.
 
-#The output of this command will be a list of unique 4-mers (from the first four nucleotides of the SEQ column of the properly paired alignments with insert size ≤ 600), along with their counts in the input BAM file.
+#The output of this command will be a list of unique 4-mers (from the first four nucleotides of the SEQ column of the properly paired forward alignments with insert size ≤ 600), along with their counts in the input BAM file.
 
-#The SEQ field in the bam field is always represented in the 5' to 3' direction (note that this means that the SEQ field for the reverse strand will be the reverse complement of the original read). For our purposes this is easier, as we need 4-mers from the 5' end only, so first 4 nuceotides of SEQ field. 
 
 samples=$1
 
 for file in $(cat $samples)
 do
-/data/leuven/338/vsc33813/miniconda3/bin/samtools view -f 2 -F 268 -q 1 data/fastq_ZOL/$file'_trimmed_aligned_merged_marked_duplicates.bam' | awk '$9<=600 && $9>=-600 && $6 !~ /S/' | cut -f 10 | cut -c 1-4 | sort | uniq -c > results/end_motifs/$file'_end_motif_freq.txt'
+/data/leuven/338/vsc33813/miniconda3/bin/samtools view -f 2 -F 284 -q 1 data/fastq_ZOL/$file'_trimmed_aligned_merged_marked_duplicates.bam' | awk '$9<=600 && $9>=-600 && $6 !~ /S/' | cut -f 10 | cut -c 1-4 | sort | uniq -c > results/end_motifs/$file'_end_motif_freq.txt'
 done
 
 
